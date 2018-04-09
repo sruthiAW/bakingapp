73c3a42d6f565a59b659a81632892ed8a330b765
ssurendran <ssurendran@vmware.com>
2018-04-09 18:29:36 +0530

    commit 4 - skeleton - activities and frags

--- app/src/main/java/com/example/ssurendran/bakingapp/activities/RecipeGridActivity.java
+++ app/src/main/java/com/example/ssurendran/bakingapp/activities/RecipeGridActivity.java
@@ -1,4 +1,4 @@
-package com.example.ssurendran.bakingapp;
+package com.example.ssurendran.bakingapp.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.ssurendran.bakingapp.R;
 
 public class RecipeGridActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/ssurendran/bakingapp/fragments/RecipeStepDetailFragment.java
@@ -0,0 +1,10 @@
+package com.example.ssurendran.bakingapp.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by ssurendran on 4/9/18.
+ */
+
+public class RecipeStepDetailFragment extends Fragment {
+}
